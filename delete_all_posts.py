8eedcb3571c0d7447c0c5daeee454c245f9a9d8a
mayukh18 <mayukh.superb@gmail.com>
2017-03-01 02:32:37 +0530

    first

--- delete_all_posts.py
+++ delete_all_posts.py
@@ -1,3 +1,15 @@
 import facebook
+import os
+import config
+
+ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN', config.ACCESS_TOKEN)
+
+
+api = facebook.GraphAPI(ACCESS_TOKEN)
+posts = api.get_connections(id='me', connection_name='posts')
+
+for post in posts['data']:
+    print(post['id'])
+    api.delete_object(id=post['id'])
 
 
